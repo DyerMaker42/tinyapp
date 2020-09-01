3e152b861c3b4aaac923b7a0d60c74cb358462a3
Nicholas Kotsos <nick.kotsos@gmail.com>
2020-09-01 05:51:24 +0000

    updated express server with body parser const

--- express_server.js
+++ express_server.js
@@ -8,6 +8,9 @@ const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
+//said needs to come before routes
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.get("/", (req, res) => {
   res.send("Hello!");
