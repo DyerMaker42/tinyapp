29fbabf9b167c932a509039cfe7167cc46b11004
Nicholas Kotsos <nick.kotsos@gmail.com>
2020-09-01 16:23:40 +0000

    changed res.send back to 'ok'

--- express_server.js
+++ express_server.js
@@ -49,6 +49,6 @@ app.get("/urls/:shortURL", (req, res) => {
 
 app.post("/urls", (req, res) => {
   console.log(req.body);  // Log the POST request body to the console
-  res.send(urlDatabase);         // Respond with 'Ok' (we will replace this)
+  res.send("ok");         // Respond with 'Ok' (we will replace this)
 });
 
