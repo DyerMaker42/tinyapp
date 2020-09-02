1abcbd2b933548606c5120cab2ff901a08da8a9f
Nicholas Kotsos <nick.kotsos@gmail.com>
2020-09-02 00:49:30 +0000

    testing

--- express_server.js
+++ express_server.js
@@ -66,3 +66,9 @@ app.get("/u/:shortURL", (req, res) => {
   const longURL = urlDatabase[req.params.shortURL]
   res.redirect(longURL);
 });
+
+app.post("/urls/:shortURL/delete", (req, res) => {
+  console.log(req.params.shortURL)
+  delete urlDatabase[req.params.shortURL]
+  res.redirect("/urls")
+})
