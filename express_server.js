9fc069f7b88b90c750601c949fe60c81181e7889
Nicholas Kotsos <nick.kotsos@gmail.com>
2020-09-03 16:00:19 +0000

    added users global object in server

--- express_server.js
+++ express_server.js
@@ -9,6 +9,20 @@ const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
+
+//user object
+const users = {
+  "userRandomID": {
+    id: "userRandomID", 
+    email: "user@example.com", 
+    password: "purple-monkey-dinosaur"
+  },
+ "user2RandomID": {
+    id: "user2RandomID", 
+    email: "user2@example.com", 
+    password: "dishwasher-funk"
+  }
+}
 //said needs to come before routes
 const bodyParser = require("body-parser");
 const cookieParser = require("cookie-parser");
